03d4f3bf45f618462c87e86df28099d46114b66a
Anurag jain <152655497+jainanurag774695@users.noreply.github.com>
2024-04-10 15:37:26 +0530

    Add files via upload
    Digital Clock

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
-const display = document.getElementById("display");
-function appendToDisplay(input){
-display.value += input;
-}
+let hrs = document.getElementById("hrs");
+let min = document.getElementById("min");
+let sec = document.getElementById("sec");
 
-function clearDisplay(){
-    display.value = "";
-}
+setInterval(()=>{
+    let currentTime = new Date();
 
-function calculate(){
-
-    try{
-        display.value = eval(display.value);
-    }
-    catch(error){
-        display.value = "Error";
-    }
+    hrs.innerHTML = (currentTime.getHours()<10?"0":"") + currentTime.getHours();
+    min.innerHTML = (currentTime.getMinutes()<10?"0":"") + currentTime.getMinutes();
+    sec.innerHTML = (currentTime.getSeconds()<10?"0":"") + currentTime.getSeconds();
 }
+,1000)
+
+
